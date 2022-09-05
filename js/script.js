c56817dfba6a4ab03526eab40c8e2e8d07e0dafb
fahad nizar <fahad.nizar@techversantinfo.com>
2022-09-05 19:39:06 +0530

    Commited

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function myFunction() {
+  alert("I am an alert box!");
+}
+
+function myFunct() {
+  alert('Hello Fahad');
+}
+function myFunction() {
+  var x = document.getElementById("myTextarea").value;
+  document.getElementById("demo").innerHTML = x;
+}
